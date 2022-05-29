7b7471b191dd006ad05016052963d60d41b555ad
Uroky1 <ilu6a234@gmail.com>
2022-05-29 18:16:08 +0400

    Ok let's go

---
+++ Beauty Force/Beauty_Force/Beauty_Force/urls.py
@@ -0,0 +1,15 @@
+
+from django.contrib import admin
+from django.urls import path
+from django.conf.urls.static import static
+from django.conf import settings
+from project.views import *
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', Main_Page.as_view(), name='main_page'),
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
