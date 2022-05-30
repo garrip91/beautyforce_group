94a5dedd4300f132d7586a047eebc8270b970c01
Uroky1 <ilu6a234@gmail.com>
2022-05-30 15:24:22 +0400

    add brands package

--- Beauty Force/Beauty_Force/project/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-from django.views import View
-
-class Main_Page(View):
-
-    def get(self, request, *args, **kwargs):
-        return render(request, 'home.html')
