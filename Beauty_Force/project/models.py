0e6dfa39ccfca3bed66c001006463ff8a83009ec
Uroky1 <ilu6a234@gmail.com>
2022-06-13 00:29:44 +0400

    add models

--- Beauty_Force/project/models.py
+++ Beauty_Force/project/models.py
@@ -39,13 +39,16 @@ class Users(AbstractUser):
 
 
 class Delivery_Addresses(models.Model):
-    recipient = models.ForeignKey('Users', on_delete=models.CASCADE, null=True, blank=True, unique=False)
+    recipient = models.ForeignKey('Users', on_delete=models.CASCADE, null=True, blank=True, unique=False, verbose_name='Заказчик')
     city = models.CharField(max_length=100, null=True, verbose_name='Город')
     street = models.CharField(max_length=100, null=True, verbose_name='Улица')
     home = models.CharField(max_length=100, null=True, verbose_name='Номер дома')
     entrance = models.CharField(max_length=100, null=True, verbose_name='Номер подъезда')
     floor = models.CharField(max_length=100, null=True, verbose_name='Этаж')
     apartment_or_office = models.CharField(max_length=100, null=True, verbose_name='Номер квартиры/Офиса')
+
+    def __str__(self):
+        return f'{self.city}'
 
     class Meta:
         verbose_name = "Адреса доставки"
@@ -60,9 +63,29 @@ class Delivery_Addresses(models.Model):
 class Brands(models.Model):
     brand_name = models.CharField(max_length=100, null=True, verbose_name='Название бренда')
 
+    def __str__(self):
+        return f'{self.brand_name}'
+
     class Meta:
         verbose_name = "Бренды"
         verbose_name_plural = "Бренды"
+
+
+"""
+Категория товара
+"""
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=200, verbose_name='Категория товара')
+    slug = models.SlugField(unique=True, null=True, verbose_name='Ссылка')
+
+    def __str__(self):
+        return f'{self.name}'
+
+    class Meta:
+        verbose_name = "Категория товара"
+        verbose_name_plural = "Категории товара"
 
 
 """
@@ -73,9 +96,15 @@ class Brands(models.Model):
 class Product(models.Model):
     brand = models.ForeignKey('Brands', on_delete=models.CASCADE, null=True, blank=True, unique=False,
                               verbose_name='Название бренда')
+    title = models.CharField(max_length=50, null=True, verbose_name='Название товара')
     about = models.TextField(max_length=1000, null=True, verbose_name='Описание')
     image = models.ImageField(blank=True, upload_to='images/product_photo/', verbose_name='Фото товара')
     price = models.DecimalField(max_digits=10, decimal_places=2, default=0, verbose_name='Цена')
+    stock = models.PositiveIntegerField(default=0, verbose_name='Остаток товара')
+    available = models.BooleanField(default=True, verbose_name='Доступность товара')
+
+    def __str__(self):
+        return f'{self.title}'
 
     class Meta:
         verbose_name = "Товар"
@@ -95,9 +124,9 @@ class Purchase_History(models.Model):
         (online, 'Онлайн покупка'),
         (offline, 'Оффлайн покупка'),
     )
-    customer = models.ForeignKey('Users', on_delete=models.CASCADE, null=True, blank=True, unique=False)
+    customer = models.ForeignKey('Users', on_delete=models.CASCADE, null=True, blank=True, unique=False, verbose_name='Заказчик')
     date = models.DateField(verbose_name='Дата доставки')
-    address = models.CharField(max_length=100, null=True, verbose_name='Адрес доставки')
+    address = models.ForeignKey('Delivery_Addresses', on_delete=models.CASCADE, null=True, blank=True, unique=False, verbose_name='Адрес доставки')
     online_or_offline = models.PositiveSmallIntegerField(choices=PURCHASE_CHOICES, blank=True, null=True, default=0,
                                                          verbose_name='Онлайн/Оффлайн')
     number_card = models.IntegerField(default=0, null=True, verbose_name='Номер карты')
@@ -105,6 +134,9 @@ class Purchase_History(models.Model):
     delivery = models.IntegerField(default=0, null=True, verbose_name='Сумма доставки')
     product = models.ManyToManyField(Product, blank=True, verbose_name='Товар')
     total_products = models.PositiveIntegerField(default=0, verbose_name='Количество товара')
+
+    def __str__(self):
+        return f'{self.address}'
 
     class Meta:
         verbose_name = "История заказов"
